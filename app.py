1f38e1fd6cdb63a802d010ede5e473ba87031e9a
ajaykhanna123 <32953636+ajaykhanna123@users.noreply.github.com>
2023-04-13 17:42:07 +0000

    update app

--- app.py
+++ app.py
@@ -10,10 +10,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import chisquare
 from scipy.stats import ks_2samp
-
 import azure_actions
 from azure_actions import upload_file_to_blob,download_blob_into_df, upload_df_to_blob, upload_file_to_blob
-
+import pickle
 
 
 app = Flask(__name__)
@@ -23,8 +22,10 @@ def upload_file():
 
         file = request.files['file']   
         df = pd.read_csv(file)
-        global orig_dtypes
+        #global orig_dtypes       
         orig_dtypes = df.dtypes.to_dict()
+        with open('TMP/orig_dtypes.pickle', 'wb') as pkl:
+            pickle.dump(orig_dtypes, pkl) 
         upload_df_to_blob('real.csv', df, azure_actions.container_client_in)
         return jsonify({"upload status":"success"})
 
@@ -42,11 +43,16 @@ def encode_data():
     params = {"anonymize_fields" :anonymize_fields,  "cat_cols" :cat_cols, "num_cols" : num_cols}   
     fake_df = pd.DataFrame()    
     ed = Encode_decode(params,real_df,fake_df)
-    global enc_df
+    #global enc_df
     enc_df = ed.encode()
+    with open('TMP/enc_df.pickle', 'wb') as pkl:
+         pickle.dump(enc_df, pkl) 
     upload_df_to_blob('enc_df.csv', enc_df, azure_actions.container_client_in)    
-    global cat_col_map
-    cat_col_map = ed.handle_cat_cols()     
+    #global cat_col_map
+    cat_col_map = ed.handle_cat_cols()
+    with open('TMP/cat_col_map.pickle', 'wb') as pkl:
+         pickle.dump(cat_col_map, pkl) 
+    
     return jsonify({"encode status":"success"})
 
 
@@ -58,10 +64,11 @@ def train_model():
     anonymize_fields = request.args.get('anonymize_fields')
     num_cols =  request.args.get('num_cols')
     cat_cols =  request.args.get('cat_cols')
-
-    enc_train_df = enc_df
-    fake_df_raw = gen(enc_train_df, samples,primary_key, anonymize_fields,num_cols, cat_cols)
-    upload_df_to_blob('fake_df_raw.csv', fake_df_raw, azure_actions.container_client_out)
+    with open('TMP/enc_df.pickle', 'rb') as handle:
+         enc_df = pickle.load(handle)
+         enc_train_df = enc_df
+         fake_df_raw = gen(enc_train_df, samples,primary_key, anonymize_fields,num_cols, cat_cols)
+         upload_df_to_blob('fake_df_raw.csv', fake_df_raw, azure_actions.container_client_out)
     return jsonify({"training status":"complete"})
 
 
@@ -75,39 +82,18 @@ def view_output():
     real_df = pd.DataFrame()
     fake_df_raw = download_blob_into_df('synth-output','fake_df_raw.csv')
     ed = Encode_decode(params,real_df,fake_df_raw)
-    fake_final =ed.decode(cat_col_map)
-    fake_final = fake_final.astype(orig_dtypes)
-    upload_df_to_blob('fake_final.csv', fake_final, azure_actions.container_client_out)
+    with open('TMP/params_dict.pickle', 'rb') as handle:
+         cat_col_map = pickle.load(handle)
+         fake_final =ed.decode(cat_col_map)
+    with open('TMP/orig_dtypes.pickle', 'rb') as handle:
+         orig_dtypes = pickle.load(handle)
+         fake_final = fake_final.astype(orig_dtypes)
+         upload_df_to_blob('fake_final.csv', fake_final, azure_actions.container_client_out)
     return jsonify({"decode status":"success"})
 
 
 @app.route('/post_processing', methods =['POST'])
 def post_processing():
-    #folder = 'Visualizations'
-    for filename in os.listdir():
-        if (filename.split('.')[-1]=='png') or (filename.split('.')[-1]=='pdf') or (filename.split('.')[-1]=='txt'):
-        #file_path = os.path.join(folder, filename)
-            try:
-                if os.path.isfile(filename) or os.path.islink(filename):
-                    os.unlink(filename)
-            except Exception as e:
-                return jsonify({"Delete Failure":'Failed to delete %s. Reason: %s' % (filename, e)})
-
-#     #folder = 'OUT'
-#     file_path = os.path.join(folder, 'statistical_comparison.txt')
-#     try:
-#         if os.path.isfile(file_path) or os.path.islink(file_path):
-#             os.unlink(file_path)
-#     except Exception as e:
-#         return jsonify({"Delete Failure":'Failed to delete %s. Reason: %s' % (file_path, e)})
-    
-#     file_path = os.path.join(folder, 'Comparison_Visual_Report.pdf')
-#     try:
-#         if os.path.isfile(file_path) or os.path.islink(file_path):
-#             os.unlink(file_path)
-#     except Exception as e:
-#         return jsonify({"Delete Failure":'Failed to delete %s. Reason: %s' % (file_path, e)})
-    
     
     file = request.files['file']
     #file2=request.files['synthetic_data']
@@ -135,6 +121,7 @@ def post_processing():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port,debug=True)
-    #app.run()
+    port = int(os.environ.get("PORT", 8000))
+    app.run(debug = True, host="0.0.0.0", port=port)
+        
+  
